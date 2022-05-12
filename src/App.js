6de55d1f693bd8ed8596caa9f3a571e122cc3876
Daniel Jimenez <djimenezsoza18@gmail.com>
2022-05-11 19:07:35 -0600

    react-route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import FormContact from './components/FormContact';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className='container mt-5'>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      </div>
-      <Footer />
+    <div>      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home /> } />
+          <Route path='/about' element={<About /> } />
+          <Route path='/services' element={<Services /> } />
+          <Route path='/contact' element={<Contact /> } />
+          <Route path='*' element={<NotFound /> } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import "./css/Header.css"
+import "./css/Header.css";
+import { Link } from 'react-router-dom';
+import Home from '../pages/Home';
 
 export default class Header extends Component {
     constructor() {
@@ -12,7 +14,8 @@ export default class Header extends Component {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                <a className="navbar-brand text-info" href="#">Daniel</a>
+                    
+                <a className="navbar-brand text-info" href="/">logo</a>
                 <button className="navbar-toggler border border-info text-info" 
                 onClick={() => {this.setState({show: !this.state.show})}}>
                 <span className="navbar-toggler-icon"></span>
@@ -21,16 +24,20 @@ export default class Header extends Component {
                 <div className={this.state.show ? 'collapse navbar-collapse' : 'collapse navbar-collapse active'}>
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">HOME</a>
+                        
+                    <a className="nav-link text-dark" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">SERVICES</a>
+                        
+                    <a className="nav-link text-dark" href="/Services">Services</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">ABOUT US</a>
+                        
+                    <a className="nav-link text-dark" href="/About">About</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">CONTACT</a>
+                        
+                    <a className="nav-link text-dark" href="/Contact">Contact</a>
                     </li>
                 </ul>
                 </div>
