7ffd3c399a864a2e1305dd0e48fb27c897d6ebee
Daniel Jimenez <djimenezsoza18@gmail.com>
2023-06-23 17:55:37 -0600

    I put navlink to the routes of the navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>      
+    <div>
       <BrowserRouter>
         <Header />
+
         <Routes>
-          <Route path='/' element={<Home /> } />
-          <Route path='/about' element={<About /> } />
-          <Route path='/services' element={<Services /> } />
-          <Route path='/contact' element={<Contact /> } />
-          <Route path='*' element={<NotFound /> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
+
         <Footer />
       </BrowserRouter>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./css/Header.css";
-import { Link } from 'react-router-dom';
-import Home from '../pages/Home';
+import logo from "../logo.svg";
+import { NavLink } from "react-router-dom";
 
 export default class Header extends Component {
-    constructor() {
-        super();
-        this.state = {
-            show: true,
-        }
-    }
-    render(){
-        return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    
-                <a className="navbar-brand text-info" href="/">logo</a>
-                <button className="navbar-toggler border border-info text-info" 
-                onClick={() => {this.setState({show: !this.state.show})}}>
-                <span className="navbar-toggler-icon"></span>
-                {/* {this.state.show} */}
-                </button>
-                <div className={this.state.show ? 'collapse navbar-collapse' : 'collapse navbar-collapse active'}>
-                <ul className="navbar-nav ms-auto">
-                    <li className="nav-item">
-                        
-                    <a className="nav-link text-dark" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        
-                    <a className="nav-link text-dark" href="/Services">Services</a>
-                    </li>
-                    <li className="nav-item">
-                        
-                    <a className="nav-link text-dark" href="/About">About</a>
-                    </li>
-                    <li className="nav-item">
-                        
-                    <a className="nav-link text-dark" href="/Contact">Contact</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-            </nav>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      show: true,
+    };
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand text-info" href="/">
+            <NavLink
+              to="/"
+              className="col-3 text-reset text-uppercase d-flex align-items-center"
+            >
+              <img src={logo} alt="" className="img-logo mr-2" height={60} />
+            </NavLink>
+          </a>
+          <button
+            className="navbar-toggler border border-info text-info"
+            onClick={() => {
+              this.setState({ show: !this.state.show });
+            }}
+          >
+            <span className="navbar-toggler-icon"></span>
+            {this.state.show}
+          </button>
+          <div
+            className={
+              this.state.show
+                ? "collapse navbar-collapse"
+                : "collapse navbar-collapse active"
+            }
+          >
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link text-dark" to="/">
+                  Inicio
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link text-dark" to="/Services">
+                  Servicios
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link text-dark" to="/About">
+                  Acerca de
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link text-dark" to="/Contact">
+                  Contacto
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
